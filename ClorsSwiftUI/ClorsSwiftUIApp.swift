f7d0975631ca5ad8995a7d441b85723d406972e0
UlianaGritchina <ugritchina@icloud.com>
2022-02-15 13:48:58 +0300

    Initial Commit

---
+++ ClorsSwiftUI/ClorsSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClorsSwiftUIApp.swift
+//  ClorsSwiftUI
+//
+//  Created by Ульяна Гритчина on 15.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct ClorsSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
